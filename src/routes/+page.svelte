2879b8c1ec3119d84f667a563537314e7d8f8e91
cantsaydorifto <108617883+cantsaydorifto@users.noreply.github.com>
2023-08-30 22:51:54 +0530

    add styles, major fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
+	let addPoint = false;
 	let score = 0;
 	type availableStatus = 'ALIVE' | 'DEAD' | 'UNKNOWN';
-	let status: availableStatus | null = null;
 	let loading = false;
 	let failed = false;
 	let charInfo: {
@@ -26,6 +26,10 @@
 		if (!charInfo) return;
 		if (status === charInfo.status.toUpperCase()) {
 			score += 10;
+			addPoint = true;
+			setTimeout(() => {
+				addPoint = false;
+			}, 500);
 			getData();
 			return;
 		}
@@ -34,56 +38,115 @@
 	}
 </script>
 
-<h1>Morty Mystery</h1>
-<!-- <h1>Status : {status ? status : 'apples'}</h1> -->
-<button on:click={getData}>Start</button>
-<h1>Score : {score}</h1>
+<main>
+	<h1>Morty Mystery</h1>
+	<!-- <h1>Status : {status ? status : 'apples'}</h1> -->
+	<h1 class={`score ${failed ? 'failed' : ''}`}>
+		Score : <span>{score}</span>
+		{#if addPoint}
+			<span class="addPoint">{'+10'}</span>
+		{/if}
+	</h1>
 
-<div class="container">
-	<div class="characterCardContainer">
-		{#if charInfo && !loading}
-			<div class="characterCard">
-				<img src={charInfo.image} alt={charInfo.name} />
-				<div>
-					<div>
-						{charInfo.name}
-					</div>
-					<div>
-						<span>Last known location:</span>
-						<span>{charInfo.location.name}</span>
-					</div>
-					<div>
-						<span>ID:</span>
-						<span>{charInfo.id}</span>
-					</div>
+	<div class="container">
+		{#if loading && !charInfo}
+			<div class="characterCard loading">
+				<p>Loading</p>
+			</div>
+			<div class="buttonContainer">Loading....</div>
+		{/if}
+		{#if !charInfo}
+			{#if !loading}
+				<button on:click={getData}>Start</button>
+			{/if}
+		{:else}
+			<div class="characterCardContainer">
+				<div class="characterCard">
+					{#if failed}
+						<span class={`characterStatus ${charInfo.status.toLocaleLowerCase()}`}
+							>{charInfo.status.toUpperCase()}</span
+						>
+					{/if}
+					{#if charInfo && !loading}
+						<img src={charInfo.image} alt={charInfo.name} />
+						<div>
+							<div>
+								{charInfo.name}
+							</div>
+							<div>
+								<span>Last known location:</span>
+								<span>{charInfo.location.name}</span>
+							</div>
+							<div>
+								<span>ID:</span>
+								<span>{charInfo.id}</span>
+							</div>
+						</div>
+					{/if}
 				</div>
 			</div>
-		{/if}
-	</div>
-	<div class="buttonContainer">
-		{#if failed}
-			<h1>FAILED</h1>
-			<button
-				on:click={() => {
-					failed = false;
-					getData();
-				}}>TRY AGAIN!</button
-			>
-		{:else}
-			<div>
-				<button on:click={() => statusHandler('ALIVE')}>Alive</button>
-				<button on:click={() => statusHandler('DEAD')}>Dead</button>
+			<div class="buttonContainer">
+				{#if failed}
+					<button
+						class="tryAgain"
+						on:click={() => {
+							failed = false;
+							getData();
+						}}>TRY AGAIN!</button
+					>
+				{:else}
+					<div>
+						<button on:click={() => statusHandler('ALIVE')}>Alive</button>
+						<button on:click={() => statusHandler('DEAD')}>Dead</button>
+					</div>
+					<button on:click={() => statusHandler('UNKNOWN')}>Unknown</button>
+				{/if}
 			</div>
-			<button on:click={() => statusHandler('UNKNOWN')}>Unknown</button>
 		{/if}
 	</div>
-</div>
-
-{#if loading}
-	<div>Loading......</div>
-{/if}
+</main>
 
 <style>
+	main {
+		width: 100%;
+		max-width: 1200px;
+		display: flex;
+		flex-direction: column;
+		gap: 50px;
+		margin: auto;
+	}
+	h1 {
+		text-align: center;
+		margin-top: 40px;
+	}
+
+	.score {
+		position: relative;
+		margin-top: 20px;
+		align-self: center;
+		width: fit-content;
+	}
+
+	.score > span:nth-child(1) {
+		display: inline-block;
+		width: 50px;
+	}
+
+	.addPoint {
+		position: absolute;
+		top: 0;
+		left: calc(100% + 10px);
+		color: rgb(89, 179, 89);
+		animation: add-point 0.3s ease-in-out;
+	}
+	@keyframes add-point {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
 	.container {
 		display: flex;
 		height: 500px;
@@ -91,6 +154,8 @@
 
 	.characterCardContainer {
 		width: 50%;
+		display: flex;
+		justify-content: center;
 	}
 	button {
 		all: unset;
@@ -117,6 +182,7 @@
 	}
 
 	.buttonContainer > div {
+		animation: buttonsPopup 0.6s ease-in-out;
 		display: flex;
 		gap: 100px;
 	}
@@ -128,10 +194,12 @@
 		background-color: rgb(187, 237, 182);
 	}
 	.buttonContainer > button {
+		animation: buttonsPopup 0.6s ease-in-out;
 		background-color: rgb(94, 94, 94);
 	}
 
 	.characterCard {
+		position: relative;
 		background-color: #303b5b;
 		margin: auto;
 		display: flex;
@@ -140,9 +208,43 @@
 		gap: 10px;
 		border-radius: 16px;
 		width: 300px;
-		height: fit-content;
-		/* box-sizing: border-box; */
+		min-height: 450px;
 		padding-bottom: 20px;
+		animation: card 0.4s ease-in-out;
+	}
+	.characterStatus {
+		position: absolute;
+		top: 10px;
+		right: 10px;
+		border-radius: 12px;
+		box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.2);
+		animation: buttonsPopup 0.6s ease-in-out;
+		padding: 10px;
+	}
+	.alive {
+		background-color: rgb(124, 216, 124);
+	}
+	.dead {
+		background-color: rgb(255, 109, 109);
+	}
+	.unknown {
+		background-color: rgb(104, 104, 104);
+	}
+	.loading {
+		align-items: center;
+		justify-content: center;
+	}
+	.loading > p {
+		font-size: 2rem;
+	}
+
+	@keyframes card {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
 	}
 
 	.characterCard > div {
@@ -150,7 +252,7 @@
 		flex-direction: column;
 		justify-content: space-between;
 		gap: 15px;
-		padding-left: 20px;
+		padding: 0 20px;
 	}
 
 	.characterCard > div > div {
@@ -175,10 +277,36 @@
 	} */
 
 	.characterCard > img {
-		height: 100%;
+		height: 300px;
 		border-top-right-radius: 16px;
 		border-top-left-radius: 16px;
 		border-bottom-left-radius: 0;
 		border-bottom-right-radius: 0;
+	}
+	.buttonContainer > .tryAgain {
+		background-color: rgb(228, 129, 129);
+		font-weight: 600;
+		animation: buttonsPopup 0.6s ease-in-out;
+	}
+	@keyframes buttonsPopup {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+	.failed {
+		animation: failed 0.2s forwards ease-in-out;
+	}
+	@keyframes failed {
+		0% {
+			color: white;
+			/* opacity: 0; */
+		}
+		100% {
+			color: rgb(255, 120, 120);
+			/* opacity: 1; */
+		}
 	}
 </style>
